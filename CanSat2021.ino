70ae7cd10dc7db271577a928b5275c75ddeffbd3
TakedaLeyasu <c.frauendorf2000@web.de>
2021-04-09 20:17:51 +0200

    Zeitstempel BMP180 TEMP36

--- CanSat2021.ino
+++ CanSat2021.ino
@@ -16,13 +16,16 @@
 #include <SPI.h>
 #include <SD.h>
 
+//BMP180
+#include <Adafruit_BMP085.h>
+Adafruit_BMP085 bmp;
 
 
-
+//****PinBelegung
 
 const int _PIN_BMP_SDA = 2;
 const int _PIN_BMP_SCL = 3;
-const int _PIN_A_Temp = 4;
+const int _PIN_TMP36 = 4;
 const int _PIN_GPS_RX = 6;
 const int _PIN_GPS_TX = 8;
 const int _PIN_CAM_EA = 7;
@@ -40,6 +43,40 @@ const int _PIN_DVR_record = 19;
    CS - siehe oben
 
 */
+//
+float ground_pressure = 101325;
+
+//****Daten2Log
+String dataString = "timestamp,system_state,bmp_pressure,bmp_alt,bmp_temp,tmp36_temp,cam_mosfet_ea,dvr_trigger,mpu_acX,mpu_acY,mpu_acZ,mpu_temp,mpu_gyX,mpu_gyY,mpu_gyZ,gps_sats,gps_VDOP,gps_HDOP,gps_PDOP,gps_Latitude,gps_Longitude,gps_FixAge,gps_Date,gps_Time,gps_DateAge,gps_altitude,gps_Course,gps_Speed,gps_Card";
+
+int system_state = 0;
+float bmp_pressure = 0;
+float bmp_alt = 0;
+float bmp_temp = 0;
+int tmp36_temp = 0;
+bool cam_mosfet_ea = 0;
+bool dvr_trigger = 0;
+float mpu_acX = 0;
+float mpu_acY = 0;
+float mpu_acZ = 0;
+float mpu_temp = 0;
+float mpu_gyX = 0;
+float mpu_gyY = 0;
+float mpu_gyZ = 0;
+float gps_sats = 0;
+float gps_VDOP = 0;
+float gps_HDOP = 0;
+float gps_PDOP = 0;
+float gps_Latitude = 0;
+float gps_Longitude = 0;
+float gps_FixAge = 0;
+float gps_Date = 0;
+float gps_Time = 0;
+float gps_DateAge = 0;
+float gps_altitude = 0;
+float gps_Course = 0;
+float gps_Speed = 0;
+float gps_Card = 0;  //????
 
 
 void setup() {
@@ -52,13 +89,38 @@ void setup() {
 #endif
 
   setup_SD();
+  setup_BMP180();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+
+  read_tmp36();
+  read_BMP180();
+
+
+
+
+
+
+
+
+  data_to_dataString();
+  dataString_to_SD();
+  delay(1000);
 }
+
+
+
+
+
+
+
+
+
+
 
 //*************Funktionen***********
 void setup_SD() {
@@ -68,24 +130,85 @@ void setup_SD() {
   if (!SD.begin(_PIN_SD_CS)) {
     DPRINTLN("Card failed, or not present");
     // don't do anything more:
-    while (1); //********************************Achtung Fehlermeldung
+    while (1); //*********************************************************************Achtung Fehlermeldung
   }
   DPRINTLN("card initialized.");
 
+  dataString_to_SD();
+}
 
 
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+void setup_BMP180() {
+  if (!bmp.begin()) {
+    DPRINTLN("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}  //*********************************************************************Achtung Fehlermeldung
+  }
+}
+
+//LOOP Funktionen
+
+
+void read_tmp36() {
+  tmp36_temp = analogRead(_PIN_TMP36);
+}
+
+void read_BMP180() {
+  bmp_pressure = bmp.readPressure();
+  bmp_alt = bmp.readAltitude(ground_pressure);
+  bmp_temp = bmp.readTemperature();
+}
+
+
+
+
+
+void data_to_dataString() {
+  dataString = "";
+  dataString += String(millis()) + ",";
+  dataString += String(system_state) + ",";
+  dataString += String(bmp_pressure) + ",";
+  dataString += String(bmp_alt) + ",";
+  dataString += String(bmp_temp) + ",";
+  dataString += String(tmp36_temp) + ",";
+  dataString += String(cam_mosfet_ea) + ",";
+  dataString += String(dvr_trigger) + ",";
+  dataString += String(mpu_acX) + ",";
+  dataString += String(mpu_acY) + ",";
+  dataString += String(mpu_acZ) + ",";
+  dataString += String(mpu_temp) + ",";
+  dataString += String(mpu_gyX) + ",";
+  dataString += String(mpu_gyY) + ",";
+  dataString += String(mpu_gyZ) + ",";
+  dataString += String(gps_sats) + ",";
+  dataString += String(gps_VDOP) + ",";
+  dataString += String(gps_HDOP) + ",";
+  dataString += String(gps_PDOP) + ",";
+  dataString += String(gps_Latitude) + ",";
+  dataString += String(gps_Longitude) + ",";
+  dataString += String(gps_FixAge) + ",";
+  dataString += String(gps_Date) + ",";
+  dataString += String(gps_Time) + ",";
+  dataString += String(gps_DateAge) + ",";
+  dataString += String(gps_altitude) + ",";
+  dataString += String(gps_Course) + ",";
+  dataString += String(gps_Speed) + ",";
+  dataString += String(gps_Card);
+}
+
+
+
+void dataString_to_SD() {
+  File dataFile = SD.open("datalog.csv", FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
-    dataFile.println("start");
+    dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
-    DPRINTLN("start");
+    DPRINTLN(dataString);
   }
   // if the file isn't open, pop up an error:
   else {
-    DPRINTLN("error opening datalog.txt");
+    DPRINTLN("error opening datalog.txt");//*********************************************************************Achtung Fehlermeldung
   }
-
 }
