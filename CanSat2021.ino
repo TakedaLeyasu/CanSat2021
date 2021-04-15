4d0f409314a07ba938fbcb8dd387c2c4bd8c61c8
TakedaLeyasu <c.frauendorf2000@web.de>
2021-04-15 21:44:33 +0200

    Add files via upload

--- CanSat2021.ino
+++ CanSat2021.ino
@@ -1,23 +1,22 @@
-#include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-int pos = 0;
-
-
-
 //*************DEBUG****************
 
-#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
-#ifdef DEBUG    //Macros are usually in all capital letters.
-#define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
-#define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
+#define DEBUG                                     //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+#ifdef DEBUG                                      //Macros are usually in all capital letters.
+#define DPRINT(...) Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
+#define DPRINTLN(...) Serial.println(__VA_ARGS__) //DPRINTLN is a macro, debug print with new line
 #else
-#define DPRINT(...)     //now defines a blank line
-#define DPRINTLN(...)   //now defines a blank line
+#warning
+#define DPRINT(...)   //now defines a blank line
+#define DPRINTLN(...) //now defines a blank line
 #endif
 
+//*******Welcher Arduino?
+//#define Arduino_1
+#define Arduino_2
+
+#if (defined(Arduino_1) == defined(Arduino_2))
+#error
+#endif
 
 //***********Bibliotheken***************
 
@@ -26,17 +25,34 @@ int pos = 0;
 #include <SD.h>
 
 //BMP180
+#ifdef Arduino_1
 #include <Adafruit_BMP085.h>
 Adafruit_BMP085 bmp;
+#endif
 
+//BMP390
+#ifdef Arduino_2
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+Adafruit_BMP3XX bmp;
+#endif
 
 //GPS
-#include <SoftwareSerial.h>
-// serial connection to the GPS device
+#ifdef Arduino_1
+#include <SoftwareSerial.h> // serial connection to the GPS device
 const int _PIN_GPS_RX = 6;
 const int _PIN_GPS_TX = 8;
 SoftwareSerial gpsSerial(_PIN_GPS_TX, _PIN_GPS_RX);
+#endif
 
+//Piper
+#ifdef Arduino_1
+#include <Servo.h>
+Servo piper; // create servo object to control a servo
+// twelve servo objects can be created on most boards
+int pos = 0;
+#endif
 
 //****PinBelegung
 
@@ -60,8 +76,14 @@ const int _PIN_DVR_record = 19;
    CS - siehe oben
 
 */
-//
-float ground_pressure = 101325;
+//Luftdruck am Boden
+#ifdef Arduino_1
+float ground_pressure = 101325; //auf standart wert gestzt -> später reset auf 0m
+#endif
+
+#ifdef Arduino_2
+float ground_pressure = 1013.25; //auf standart wert gestzt -> später reset auf 0m
+#endif
 
 //****Daten2Log
 String dataString = "timestamp,system_state,bmp_pressure,bmp_alt,bmp_temp,tmp36_temp,cam_mosfet_ea,dvr_trigger,mpu_acX,mpu_acY,mpu_acZ,mpu_temp,mpu_gyX,mpu_gyY,mpu_gyZ,gps_sats,gps_VDOP,gps_HDOP,gps_PDOP,gps_Latitude,gps_Longitude,gps_FixAge,gps_Date,gps_Time,gps_DateAge,gps_altitude,gps_Course,gps_Speed,gps_Card";
@@ -93,122 +115,188 @@ float gps_DateAge = 0;
 float gps_altitude = 0;
 float gps_Course = 0;
 float gps_Speed = 0;
-float gps_Card = 0;  //????
+float gps_Card = 0; //????
 
-
-void setup() {
+void setup()
+{
 #ifdef DEBUG
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  while (!Serial) {
+  while (!Serial)
+  {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 #endif
 
   setup_SD();
+
+#ifdef Arduino_1
   setup_BMP180();
+
   gpsSerial.begin(9600);
 
+  piper.attach(9);
+#endif
 
-
-  myservo.attach(9);
-
+#ifdef Arduino_2
+  setup_BMP390();
+  //setup_MPU();
+#endif
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
 
-  for (pos = 0; pos <= 180; pos += 50) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-                           // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 50) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-                           // waits 15ms for the servo to reach the position
-  }
-
-
-
-
   read_tmp36();
+
+#ifdef Arduino_1
   read_BMP180();
   read_GPS();
+  mute_piper();
+#endif
 
-
-
-
-
-
+#ifdef Arduino_2
+  read_BMP390();
+  //read_MPU();
+#endif
 
   data_to_dataString();
   dataString_to_SD();
   //delay(1000);
 }
 
-
-
-
-
-
-
-
-
-
-
 //*************Funktionen***********
-void setup_SD() {
+void setup_SD()
+{
   DPRINT("Initializing SD card...");
 
   // see if the card is present and can be initialized:
-  if (!SD.begin(_PIN_SD_CS)) {
+  if (!SD.begin(_PIN_SD_CS))
+  {
     DPRINTLN("Card failed, or not present");
     // don't do anything more:
-    while (1); //*********************************************************************Achtung Fehlermeldung
+    while (1)
+      ; //*********************************************************************Achtung Fehlermeldung
   }
   DPRINTLN("card initialized.");
 
   dataString_to_SD();
 }
 
-
-void setup_BMP180() {
-  if (!bmp.begin()) {
+#ifdef Arduino_1
+void setup_BMP180()
+{
+  if (!bmp.begin())
+  {
     DPRINTLN("Could not find a valid BMP085 sensor, check wiring!");
-    while (1) {}  //*********************************************************************Achtung Fehlermeldung
+    while (1)
+    {
+    } //*********************************************************************Achtung Fehlermeldung
   }
+  read_BMP180();
+  ground_pressure = bmp_pressure;
 }
+#endif
 
-
+#ifdef Arduino_2
+void setup_BMP390()
+{
+  if (!bmp.begin_I2C())
+  { // hardware I2C mode, can pass in address & alt Wire
+    DPRINTLN("Could not find a valid BMP3 sensor, check wiring!");
+    while (1)
+      ; //*********************************************************************Achtung Fehlermeldung
+  }
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+  read_BMP390();
+  ground_pressure = bmp_pressure/100;
+}
+#endif
 
 //LOOP Funktionen
 
-
-void read_tmp36() {
+void read_tmp36()
+{
   tmp36_temp = analogRead(_PIN_TMP36);
 }
 
-void read_BMP180() {
+#ifdef Arduino_1
+void read_BMP180()
+{
   bmp_pressure = bmp.readPressure();
   bmp_alt = bmp.readAltitude(ground_pressure);
   bmp_temp = bmp.readTemperature();
 }
+#endif
 
-void read_GPS() {
-  // Output raw GPS data to the serial monitor
-  if(gpsSerial.available()>0){
-    Serial.println("GPS:");
-  while (gpsSerial.available() > 0) {
-    Serial.write(gpsSerial.read());
+#ifdef Arduino_2
+void read_BMP390()
+{
+  if (!bmp.performReading())
+  {
+    DPRINTLN("Failed to perform reading :(");
+    return;
   }
-  Serial.println();
+  bmp_pressure = bmp.pressure;
+  bmp_alt = bmp.readAltitude(ground_pressure);
+  bmp_temp = bmp.temperature;
+}
+#endif
+
+#ifdef Arduino_1
+void read_GPS()
+{
+  // Output raw GPS data to the serial monitor
+  if (gpsSerial.available() > 0)
+  {
+    Serial.println("GPS:");
+    File dataFile = SD.open("datalog.gps", FILE_WRITE);
+
+    // if the file is available, write to it:
+    if (dataFile)
+    {
+      while (gpsSerial.available() > 0)
+      {
+        //Serial.write(gpsSerial.read());
+        dataFile.write(gpsSerial.read());
+      }
+      dataFile.close();
+      // print to the serial port too:
+      DPRINTLN("GPS geschrieben");
+    }
+    // if the file isn't open, pop up an error:
+    else
+    {
+      DPRINTLN("error opening datalog.txt"); //*********************************************************************Achtung Fehlermeldung
+    }
   }
 }
+#endif
 
+#ifdef Arduino_1
+void mute_piper()
+{
+  for (pos = 0; pos <= 180; pos += 50)
+  { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    piper.write(pos); // tell servo to go to position in variable 'pos'
+    // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 50)
+  {                   // goes from 180 degrees to 0 degrees
+    piper.write(pos); // tell servo to go to position in variable 'pos'
+    // waits 15ms for the servo to reach the position
+  }
+}
+#endif
 
-
-
-void data_to_dataString() {
+void data_to_dataString()
+{
   dataString = "";
   dataString += String(millis()) + ",";
   dataString += String(system_state) + ",";
@@ -241,20 +329,21 @@ void data_to_dataString() {
   dataString += String(gps_Card);
 }
 
-
-
-void dataString_to_SD() {
+void dataString_to_SD()
+{
   File dataFile = SD.open("datalog.csv", FILE_WRITE);
 
   // if the file is available, write to it:
-  if (dataFile) {
+  if (dataFile)
+  {
     dataFile.println(dataString);
     dataFile.close();
     // print to the serial port too:
     DPRINTLN(dataString);
   }
   // if the file isn't open, pop up an error:
-  else {
-    DPRINTLN("error opening datalog.txt");//*********************************************************************Achtung Fehlermeldung
+  else
+  {
+    DPRINTLN("error opening datalog.txt"); //*********************************************************************Achtung Fehlermeldung
   }
 }
